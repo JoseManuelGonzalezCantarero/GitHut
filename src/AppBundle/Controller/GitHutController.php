4d491a34b60324468f84cc5a4df07e882b5789b2
Jose Gonzalez <jose.gonzalez@webfg.com>
2016-06-21 13:05:54 +0200

    Dynamic Repositories and Other Geekery

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -9,51 +9,32 @@ use Symfony\Component\HttpFoundation\Request;
 class GitHutController extends Controller
 {
     /**
-     * @Route("/", name="githut")
+     * @Route("/{username}", name="githut", defaults={"username": "JoseManuelGonzalezCantarero"})
      */
-    public function githutAction(Request $request)
+    public function githutAction(Request $request, $username)
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.github.com/users/JoseManuelGonzalezCantarero');
-        $data = json_decode($response->getBody()->getContents(), true);
-        $templateData = [
-            'avatar_url'  => $data['avatar_url'],
-            'name'        => $data['name'],
-            'login'       => $data['login'],
-            'details'     => [
-                'company'   => $data['company'],
-                'location'  => $data['location'],
-                'joined_on' => 'Joined on ' . (new \DateTime($data['created_at']))->format('d m Y'),
-            ],
-            'blog' => $data['blog'],
-            'social_data' => [
-                'followers'    => $data['followers'],
-                'following'    => $data['following'],
-                'public_repos' => $data['public_repos'],
-            ],
-            'repo_count' => 100,
-            'most_stars' => 50,
-            'repos' => [
-                [
-                    'url' => 'https://codereviewvideos.com',
-                    'name' => 'Code Review Videos',
-                    'description' => 'some repo description',
-                    'stargazers_count' => '999',
-                ],
-                [
-                    'url' => 'http://bbc.co.uk',
-                    'name' => 'The BBC',
-                    'description' => 'not a real repo',
-                    'stargazers_count' => '666',
-                ],
-                [
-                    'url' => 'http://google.co.uk',
-                    'name' => 'Google',
-                    'description' => 'another fake repo description',
-                    'stargazers_count' => '333',
-                ],
-            ],
-        ];
-        return $this->render('githut/index.html.twig', $templateData);
+        return $this->render('githut/index.html.twig', [
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * @Route("/profile/{username}", name="profile")
+     */
+    public function profileAction(Request $request, $username)
+    {
+        $profile = $this->get('github_api')->getProfile($username);
+        
+        return $this->render('githut/profile.html.twig', $profile);
+    }
+
+    /**
+     * @Route("/repos/{username}", name="repos")
+     */
+    public function reposAction(Request $request, $username)
+    {
+        $repos = $this->get('github_api')->getRepositories($username);
+        
+        return $this->render('githut/repos.html.twig', $repos);
     }
 }
